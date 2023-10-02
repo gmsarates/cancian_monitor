8024d1283dfd418f91ad63fd2ac64d66364db1ab
Gabriel Sarates <30908688+gmsarates@users.noreply.github.com>
2023-10-02 20:05:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,42 +8,48 @@ const vgmUrl= 'https://www.fernandocancian.com/imovel/?tipo=casa&finalidade=loca
 
 let last_text = null;
 
-setTimeout(() => {
+setInterval(() => {
     got(vgmUrl).then(response => {
-      const dom = new JSDOM(response.body);
-      let result = dom.window.document.querySelector('.lista_imoveis_paginacao .fontepadrao_cor_grande').textContent;
-      let num = result.match(/(\d+)/g);
-      num = num[0];
+        console.log(new Date());
+        console.log('Consultando site...');
+        const dom = new JSDOM(response.body);
+        let result = dom.window.document.querySelector('.lista_imoveis_paginacao .fontepadrao_cor_grande').textContent;
+        let num = result.match(/(\d+)/g);
+        num = num[0];
 
-      if (last_text == null || result !== last_text) {
-        last_text = result;
+        console.log(num, 'imóveis cadastrados');
+        if (last_text == null || result !== last_text) {
+            console.log('Enviando via Whatsapp');
+            last_text = result;
 
-        request.post(
-            'http://62.72.8.127:3333/message/text?key=CANCIAN', { json: {
-                    "id": "555191155222",
-                    "message": `Número de imóveis no site FERNANDO CANCIAN alterado: *${num} imóveis*`
-                } 
-            },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(body);
+            request.post(
+                'http://62.72.8.127:3333/message/text?key=CANCIAN', { json: {
+                        "id": "555191155222",
+                        "message": `Número de imóveis no site FERNANDO CANCIAN alterado: *${num} imóveis*`
+                    } 
+                },
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log(body);
+                    }
                 }
-            }
-        );
+            );
 
-        request.post(
-            'http://62.72.8.127:3333/message/text?key=CANCIAN', { json: {
-                    "id": "555597157763",
-                    "message": `Número de imóveis no site FERNANDO CANCIAN alterado: *${num} imóveis*`
-                } 
-            },
-            function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    console.log(body);
-                }
-            }
-        );
-    }
+            // request.post(
+            //     'http://62.72.8.127:3333/message/text?key=CANCIAN', { json: {
+            //             "id": "555597157763",
+            //             "message": `Número de imóveis no site FERNANDO CANCIAN alterado: *${num} imóveis*`
+            //         } 
+            //     },
+            //     function (error, response, body) {
+            //         if (!error && response.statusCode == 200) {
+            //             console.log(body);
+            //         }
+            //     }
+            // );
+        }
+
+        console.log('FIM', '');
     }).catch(err => {
       console.log(err);
     });
